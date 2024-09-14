456361958f5d123b1a16c52436ee4eb574b79072
vovapr <press.vladimir@gmail.com>
2024-09-15 02:52:39 +0300

    Added ConnectionFactory

--- src/main/java/com/useradmin/configMq/RabbitConfig.java
+++ src/main/java/com/useradmin/configMq/RabbitConfig.java
@@ -4,19 +4,40 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 
 @Configuration
 public class RabbitConfig {
 
     public static final String QUEUE_NAME = "userQueue";
 
+    @Value("${spring.rabbitmq.addresses}")
+    private String addresses;
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setUri(addresses);
+        return connectionFactory;
+    }
+
     @Bean
     public Queue userQueue() {
         return new Queue(QUEUE_NAME, true);
     }
-
-    @Bean
-    public RabbitTemplate rabbitTemplate() {
-        return new RabbitTemplate();
-    }
+//
+//    @Bean
+//    public RabbitTemplate rabbitTemplate() {
+//        return new RabbitTemplate();
+//    }
+//
+//    @Bean
+//    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+//        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+//        rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+//        return rabbitTemplate;
+//    }
 }
